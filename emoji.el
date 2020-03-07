92e78fd952c429f7ff68addab4781ee6a1571a13
Tsuyoshi Kitamoto <tsuyoshi.kitamoto@gmail.com>
2020-03-07 14:20:33 +0900

    Add Copyright

--- emoji.el
+++ emoji.el
@@ -1,5 +1,21 @@
 ;;; emoji.el ---   -*- coding: utf-8 -*-
 
+;; Copyright (C) 2020 Tsuyoshi Kitamoto  <tsuyoshi.kitamoto@gmail.com>
+;; Author: 2020 Tsuyoshi Kitamoto  <tsuyoshi.kitamoto@gmail.com>
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program, If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;; ファイル https://unicode.org/emoji/charts-13.0/emoji-list.html を基に、
 ;; skk 辞書のエントリー形式を標準出力へ吐き出します。
